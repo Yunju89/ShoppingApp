6cd0289b08dd5164e47cf09345af2491359f49af
Yunju89 <toyaann@naver.com>
2022-03-18 20:52:09 +0900

    #2 상품 리스트 뷰 만들기  - TabLayout 제작

--- app/src/main/java/org/techtown/shoppingapp/fragments/HomeFragment.kt
+++ app/src/main/java/org/techtown/shoppingapp/fragments/HomeFragment.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import org.techtown.shoppingapp.R
+import org.techtown.shoppingapp.adapters.CategoryAdapter
 import org.techtown.shoppingapp.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment() {
 
     lateinit var binding : FragmentHomeBinding
+
+    lateinit var mCategoryAdapter : CategoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +37,24 @@ class HomeFragment : BaseFragment() {
     }
 
     override fun setValues() {
+
+        val dataArr = arrayOf(
+            "가전디지털",
+            "뷰티",
+            "생활용품",
+            "식품",
+            "주방용품",
+            "캠핑",
+            "패션의류/잡화",
+            "홈인테리어"
+        )
+
+        mCategoryAdapter = CategoryAdapter(parentFragmentManager, dataArr)
+
+        binding.homeViewPager.adapter = mCategoryAdapter
+
+        binding.homeTabLayout.setupWithViewPager(binding.homeViewPager)
+
 
     }
 
