402df4d1029de27f9f9d02abf56e890f002d2454
Yunju89 <toyaann@naver.com>
2022-03-21 14:37:26 +0900

    #3 상세 페이지 xml 그리기

--- app/src/main/java/org/techtown/shoppingapp/ProductDetailActivity.kt
+++ app/src/main/java/org/techtown/shoppingapp/ProductDetailActivity.kt
@@ -2,10 +2,24 @@ package org.techtown.shoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.shoppingapp.databinding.ActivityProductDetailBinding
 
-class ProductDetailActivity : AppCompatActivity() {
+class ProductDetailActivity : BaseActivity() {
+    lateinit var binding : ActivityProductDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_product_detail)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
