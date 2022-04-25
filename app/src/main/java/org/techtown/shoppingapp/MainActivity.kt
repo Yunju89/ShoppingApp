6e41654bffbdd120ae9bf7d3b5594f9f1bf4f580
Yunju89 <toyaann@naver.com>
2022-04-25 18:34:00 +0900

    #11 홈에서 뒤로가기 버튼 클릭 시 토스트메시지

--- app/src/main/java/org/techtown/shoppingapp/MainActivity.kt
+++ app/src/main/java/org/techtown/shoppingapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package org.techtown.shoppingapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import org.techtown.shoppingapp.adapters.MainViewPagerAdapter
 import org.techtown.shoppingapp.databinding.ActivityMainBinding
@@ -81,4 +84,23 @@ class MainActivity : BaseActivity() {
         binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)
 
     }
+
+    var time : Long = 0
+    override fun onBackPressed() {
+
+        val time1 = System.currentTimeMillis()
+
+        val time2 = time1 - time
+
+        if(time2 in 0 .. 2000){
+            finish()
+        }
+        else {
+            time = time1
+            Toast.makeText(mContext, "'뒤로' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
 }
